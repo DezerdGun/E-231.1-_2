e0640afa1217771631b5974bb137bb3dbd40bbe4
Azambek Jurayev <JurayevA@corp.itstep>
2023-07-10 21:48:32 +0500

    siu!

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print("hello")
+with open("README.md", "r")as file:
+    content = file.read()
+    words = content.split()
+    word_count = len(words)
+    print("Количество слов в файле:", word_count)
